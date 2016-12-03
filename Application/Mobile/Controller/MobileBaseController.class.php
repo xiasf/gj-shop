9872595ba28b475ae9b8bc33a477dfdeb9f953d0
xiasf <811800545@qq.com>
2016-12-03 12:20:58 +0800

    test

--- Application/Mobile/Controller/MobileBaseController.class.php
+++ Application/Mobile/Controller/MobileBaseController.class.php
@@ -45,7 +45,7 @@ class MobileBaseController extends Controller
             $wechat_config       = $wechat_list[0];
             $this->weixin_config = $wechat_config;
             $this->assign('wechat_config', $wechat_config); // 微信配置
-            var_dump($_SESSION['openid']);
+            var_dump($_SESSION);
             if ($wechat_config && !$_SESSION['openid']) {
                 //去授权获取openid
                 $wxuser = $this->GetOpenid();
