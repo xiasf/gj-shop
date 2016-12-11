40e853b5c6454f75bec6bf631d54e96dc5797caf
xiasf <811800545@qq.com>
2016-12-11 20:06:46 +0800

    up

--- Application/Mobile/Controller/IndexController.class.php
+++ Application/Mobile/Controller/IndexController.class.php
@@ -17,7 +17,7 @@ class IndexController extends MobileBaseController
 
     function test()
     {
-        include '/a.php';
+        require '/a.php';
         echo get_thum_images('/Public/upload/ad/2016/12-11/584d14dc412ac.jpg', 160, 160);
     }
 
